05d2119140c2a7f65c1eb8c6bd9260847373b7ae
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2022-05-17 19:20:42 +0900

    boj 1789

---
+++ src/boj_1789/Main.java
@@ -0,0 +1,31 @@
+package boj_1789;
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        long S = Long.parseLong(br.readLine());
+        
+        long sum = 0;
+        long N = 1;
+        
+        while(sum < S){
+            sum += N;
+            N++;
+        }
+        
+        if(sum == S)
+        	bw.write(N-1+"");
+        else
+        	bw.write(N-2+"");
+        
+        br.close();
+        bw.flush();
+        bw.close();
+        
+    }
+}

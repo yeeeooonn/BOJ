220664a80af5ffcf4e0f51360db4a9f40dcb6f27
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2022-05-27 11:11:42 +0900

    boj 2164

---
+++ src/boj_2164/Main.java
@@ -0,0 +1,27 @@
+package boj_2164;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i = 1; i <= N; i++)
+			q.offer(i);
+		
+		while(q.size() !=1) {
+			q.poll();
+			q.offer(q.poll());
+		}
+
+		bw.write(q.peek()+"");
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
+

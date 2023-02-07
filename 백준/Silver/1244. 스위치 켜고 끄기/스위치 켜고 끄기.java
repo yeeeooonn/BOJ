d22bf659169c8195c4c4f45efe1aa72e1cf24cb3
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-02-07 17:27:30 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 80 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -45,20 +45,13 @@ public class Main {
 
 	}
 	static void girl(int i) { //여학생
-		if(s[i]==1) {
-			s[i]=0;
-		} else s[i]=1;
+		s[i] = (s[i]==1)? 0 :1; 
 		
 		int j =1;
 		if(i-j <0 || i+j>=N) return;
 		while(s[i-j] == s[i+j]) {
-			if(s[i-j]==1) {
-				s[i-j]=0;
-				s[i+j]=0;
-			}else {
-				s[i-j]=1;
-				s[i+j]=1;
-			}
+			s[i-j] = (s[i-j]==1)? 0 : 1; 
+			s[i+j] = (s[i+j]==1)? 0 : 1; 
 			j++;
 			if(i-j <0 || i+j>=N) return;
 		}
@@ -66,8 +59,7 @@ public class Main {
 	static void boy(int i) { //남학생
 		for (int j = 0; j < N; j++) {
 			if((j+1)%i ==0) {
-				if(s[j] == 1) s[j]=0;
-				else s[j]=1;
+				s[j] = (s[j] == 1)? 0 : 1;
 			}
 		}
 	}

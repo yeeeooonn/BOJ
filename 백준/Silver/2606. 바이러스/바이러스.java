ab22d7ceed424069a945038943a7c8d442b835e2
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-02-22 11:44:57 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11720 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,53 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+//List 사용
 public class Main {
-	static int N;
-	static int[] com;
-	static boolean[] visit;
-	static int[][] road;
-	static int count;
-	public static void main(String[] args) throws Exception {
+	static int N,M;
+	static List<Integer>[] lists;
+	static boolean[] visited;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		com = new int[N+1];
-		visit = new boolean[N+1];
-		road = new int[N+1][N+1];
-		int M = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		lists = new ArrayList[N];
+		visited = new boolean[N];
+		
 		for (int i = 0; i < M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			road[x][y] =1;
-			road[y][x] =1;
+			int s = Integer.parseInt(st.nextToken())-1;
+			int e = Integer.parseInt(st.nextToken())-1;
+			if(lists[s] == null) lists[s] = new ArrayList<>();
+			if(lists[e] == null) lists[e] = new ArrayList<>();
+			lists[s].add(e);
+			lists[e].add(s);
 		}
-		bfs();
-		System.out.println(count);
+		bfs(0);
+		result();
 	}
-	static void bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[]{1,0});
-		visit[1] = true;
+	static void result() {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			if(visited[i]) cnt++;
+		}
+		System.out.println(cnt-1);
+	}
+	static void bfs(int s) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(s);
+		visited[s] = true;
 		while(!q.isEmpty()) {
-			int a[] = q.poll();
-			for (int d = 1; d < N+1; d++) {
-				int nr = a[0];
-				int nc = d;
-				
-				if(!check(nr,nc) || visit[nc]) continue;
-				if(road[nr][nc] == 1) {
-					count++;
-					q.offer(new int[] {nc,0});
-					visit[nc] = true;
-				}
+			int start = q.poll();
+			for (int e : lists[start]) {
+				if(visited[e]) continue;
+				q.offer(e);
+				visited[e] = true;
 			}
 		}
 	}
-	static boolean check(int nr, int nc) {
-		return nr>=0 && nc>=0 && nr<N+1 && nc<N+1;
-	}
-
 }

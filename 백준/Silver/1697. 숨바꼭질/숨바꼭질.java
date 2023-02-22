f086ce0718ca916a6161098b61c59806e0760845
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-02-22 09:52:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int []arr = new int[100001];
+		Queue<Integer> q = new LinkedList<>();
+		
+		
+		q.offer(N);
+		
+		while(!q.isEmpty()) {
+			int a = q.poll();
+			
+			if(a == K)
+				break;
+				
+			if(a-1 >= 0 && arr[a-1] == 0) {
+				q.offer(a-1);
+				arr[a-1] =arr[a]+1;
+			}
+			if(a+1 <=100000 && arr[a+1] == 0) {
+				q.offer(a+1);
+				arr[a+1] =arr[a]+1;
+			}
+			if(2*a <= 100000 && arr[2*a] == 0) {
+				q.offer(2*a);
+				arr[2*a] =arr[a]+1;
+			}
+			
+
+		}
+		
+		System.out.print(arr[K]);
+	
+	}
+}

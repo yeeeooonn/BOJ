e9f00841d775d943a6de030ce59251d138e266b9
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-03-04 17:44:01 +0900

    [Silver II] Title: 좌표 압축, Time: 1344 ms, Memory: 229528 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,37 +1,59 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+//이분탐색이용
 public class Main {
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        int arr[] = new int[N];
-        int clone[] = new int[N]; //복제 배열
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str, " ");
-        
-        for(int i = 0; i<N; i++) {
-            arr[i] = clone[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(clone);
-        
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        int count =0;
-        
-        for(int i = 0; i<clone.length; i++) {
-            if(!hmap.containsKey(clone[i])) //중복없이
-            	hmap.put(clone[i], count++); //정렬된 배열 순으로 값을 넣고 카운트 증가
-        }
-        for(int i =0; i<N; i++) {
-        	bw.write(hmap.get(arr[i])+" "); //기존 배열값을 해시맵의 키값으로 사용해서 카운트 출력
-        }
-        
-        br.close();
-        
-        bw.flush();
-        bw.close();
-    }
+	static int N;
+	static int index;
+	static int nums[];
+	static int numsCopy[]; //기존 배열 순서 저장
+	static int numsDis[]; //중복 제거 배열
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N];
+		numsCopy = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+			numsCopy[i] = nums[i];
+		}
+		Arrays.sort(nums);
+		int acc = nums[0];
+		//0~ index 까지 배열 -> 중복제거
+		index = 1;
+		for (int i = 1; i < N; i++) {
+			if(acc != nums[i]) {
+				nums[index++] = nums[i];
+				acc = nums[i];
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			sb.append(find(numsCopy[i])).append(" ");
+		}
+		System.out.println(sb.toString());
+		
+
+	}
+	static int find(int target) {
+		int s = 0;
+		int e = index-1;
+		while(s<=e) {
+			int mid = (s+e)/2;
+			if(target < nums[mid]) {
+				e = mid-1;
+			}
+			if(target > nums[mid]) {
+				s = mid+1;
+			}
+			if(target == nums[mid]) {
+				return mid;
+			}
+		}
+		return 0;
+	}
+
 }

b8d75e48b935d578f96ee3750b1a2d48fb799680
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-03-05 22:24:36 +0900

    [Gold V] Title: 수 고르기, Time: 260 ms, Memory: 22588 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -3,10 +3,10 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//투포인터
 public class Main {
 	static int N,M;
 	static int[] nums;
-	static int result= Integer.MAX_VALUE;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,30 +18,22 @@ public class Main {
 		}
 		Arrays.sort(nums);
 		
-		int start = 0, end = 0;
-        int mid = 0;
-        for (int i = 0; i < N; i++) {
-            start = i;
-            end = N;
-            while (start != N) {
-                mid = (start + end) / 2;
- 
-                if (nums[mid] - nums[i] == M) {
-                    result = M;
-                    break;
-                }
-                if (nums[mid] - nums[i] < M) {
-                    start = mid + 1;
-                } else if (start == end) {
-                	result = Math.min(result, nums[start] - nums[i]);
-                    break;
-                } else {
-                    end = mid;
-                }
-                
-            }
-        }
+		int start = 0;
+		int end = 0;
+		int result = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < N; i++) {
+			start = i;
+			while(nums[end] - nums[start] < M) {
+				if(end == N-1) break;
+				end++;
+			}
+			if(nums[end] - nums[start] >= M) {
+				result = Math.min(result, nums[end] - nums[start]);
+			}
+		}
 		System.out.println(result);
+
 	}
 
 }

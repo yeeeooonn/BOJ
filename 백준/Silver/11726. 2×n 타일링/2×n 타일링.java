6f0920a69b4a12643f712a4fb590a769b5c78cfc
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-03-28 14:31:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N;
+	static int[] dp;
+	static int MOD = 10007;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		dp = new int[N+1];
+		dp[0] = dp[1] = 1;
+		for (int i = 2; i < N+1; i++) {
+			dp[i] = (dp[i-2] + dp[i-1])%MOD;
+		}
+		
+		System.out.println(dp[N]);
+	}
+
+}

e4b28be908aee42bf9230a5e635a7064a57fed3d
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-03-31 12:55:24 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 80 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,33 +1,62 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
 	static int[] nums;
-	static int[] dp;
+	static int[] binary;
 	static int result;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		nums = new int[N+1];
-		dp = new int[N+1];
+		nums = new int[N];
+		binary = new int[N];
+		
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		int index = 0; //이분탐색 인덱스(마지막)
 		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 0; j < i; j++) {
-				if(nums[i] > nums[j]) {
-					dp[i] = Math.max(dp[i], dp[j]+1);
-					result = Math.max(result, dp[i]);
+		binary[0] = nums[0];
+		for (int i = 1; i < N; i++) {
+/*			System.out.println("i=>" + i+" "+nums[i]);
+			for (int j = 0; j < N; j++) {
+				System.out.print(binary[j]+" ");
+			}
+			System.out.println();*/
+			if(nums[i] > binary[index]) {
+				binary[++index] = nums[i];
+			}else {
+				int temp = Arrays.binarySearch(binary, 0, index, nums[i]); //위치반환
+				if(temp < 0) {
+					temp = Math.abs(temp+1);
 				}
+				binary[temp] = nums[i];
 			}
 		}
-		System.out.println(result);
+		
+		
+/*		for (int i = 0; i < N; i++) {
+			System.out.print(binary[i]+" ");
+		}
+		System.out.println();*/
+		
+		//binary 크기 계산
+		for (int i = N-1; i >= 0; i--) {
+			if(binary[i] != 0) {
+				System.out.println(i+1);
+				break;
+			}
+		}
+		
+		
+
 	}
 
 }

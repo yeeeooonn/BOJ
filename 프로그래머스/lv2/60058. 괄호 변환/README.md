b672fc89525c20f8c5814f7a39bcd4d38d4de6ab
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-08-22 08:59:07 +0900

    [level 2] Title: 괄호 변환, Time: 2.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/README.md
+++ 프로그래머스/lv2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058?language=java) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 3.86 ms
+메모리: 71.4 MB, 시간: 2.03 ms
 
 ### 구분
 
--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
@@ -11,18 +11,15 @@ class Solution {
             return;
         }
         boolean leftC = false; //왼쪽 문자열이 올바른 괄호 문자열인지
-        boolean rightC = false; //오른쪽 문자열이 올바른 괄호 문자열인지
         
         int point = 0;
         int same = 0; //왼괄호,오른괄호 갯수 -: 왼괄호 많음 , 0:같음, +: 오른괄호 많음
+        
         if(str.charAt(0)=='(') { //왼괄호 시작일 때
-            same = -1;
             leftC = true;
         }
-        if(str.charAt(0)==')') { //오른괄호 시작일 때
-            same = 1;
-        }
-        for(int i = 1; i < str.length(); i ++) {
+
+        for(int i = 0; i < str.length(); i ++) {
             if(str.charAt(i) == '(') { //왼괄호인 경우
                 same-=1;
             }
@@ -34,18 +31,11 @@ class Solution {
                 break;
             }
         }
-        System.out.println(leftC);
+
         if(leftC) {
             answer += str.substring(0,point+1);
             divide(str.substring(point+1,str.length()));
         }
-        //이사이에 재귀
-//         System.out.println(point);
-//         if(point < str.length()) {
-            
-//             System.out.println(str.substring(point+1,str.length()));
-//             divide(str.substring(point+1,str.length()));
-//         }
         
         if(!leftC) {
             answer += '(';

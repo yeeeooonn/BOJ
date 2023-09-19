740f391b5322b30aaf82a8c6d099064732676977
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2023-09-19 21:04:56 +0900

    [level 3] Title: 징검다리 건너기, Time: 46.63 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int solution(int[] stones, int k) {
+        int left = 1;
+        int right = 200000000;
+        
+        while(true) {
+            int mid = (left+right)/2;
+            if(left > right) {
+                return left-1;
+            }
+        
+            System.out.println(left+" "+right+" "+mid);
+            System.out.println(isPossible(mid,stones,k));
+            
+            if(isPossible(mid,stones,k)) {
+                left = mid+1;
+            }else {
+                right = mid-1;
+            }
+            
+            
+        }
+    }
+    
+    static boolean isPossible(int num, int[] stones, int k) {
+            //num보다 이하인게 연속으로 k개 있으면 안된다.
+            int temp = 0;
+            for(int i = 0; i < stones.length; i++) {
+                if(stones[i] < num) {
+                    temp ++;
+                }else {
+                    temp = 0;
+                }
+
+                if(temp == k ) {
+                    return false;
+                }
+            }
+            return true;
+    }
+     
+    
+
+}

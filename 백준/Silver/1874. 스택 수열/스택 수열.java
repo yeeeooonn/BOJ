996cc9e5b1db8f41d95b94894de2e6d5d9326125
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2024-01-06 17:58:44 +0900

    [Silver II] Title: 스택 수열, Time: 348 ms, Memory: 27632 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> s = new Stack<>();
+		
+		
+		int start = 0;
+		
+		for(int i = 0; i < N; i ++) {
+			int a = Integer.parseInt(br.readLine());
+			
+			if(start < a) {
+				for(int j = start+1; j <= a; j ++) {
+					s.push(j);
+					sb.append('+').append('\n');
+					start +=1;
+				}
+				
+			}else if (s.peek()!=a) {
+				System.out.println("NO");
+				return;
+			}
+			
+			s.pop();
+			sb.append('-').append('\n');
+		}
+		
+		System.out.println(sb);
+		
+	}
+}

e46df9a7cfa9b2d7961a771f5d9ba55dac6fe575
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2024-02-04 23:25:48 +0900

    [Gold IV] Title: N-Queen, Time: 5220 ms, Memory: 12080 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,45 +2,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int N;
-	static int[] chessCol;
-	static int result; //결과값
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		chessCol = new int[N]; //인덱스 : 행, 값: 열
-		result = 0;
-		
-		queen(0);
-		
-		System.out.println(result);
+    static int N;
+    static int[] board;
+    static int answer;
 
-	}
-	static void queen(int depth) {
-		//모두 탐색했을 경우
-		if(depth == N) {
-			result++;
-			return;
-		}
-		
-		//해당 열의 모든 경우를 탐색 & 놓을 수 있으면 다음 열로
-		for (int i = 0; i < N; i++) {
-			chessCol[depth] = i;
-			if(isPossible(depth))
-				queen(depth+1);
-		}
-	}
-	static boolean isPossible(int col) {
-		for (int i = 0; i < col; i++) {
-			//해당 열과 i열의 행이 일치 -> false(세로 겹침)
-			if(chessCol[col] == chessCol[i])
-				return false;
-			//대각선(기울기 절댓값이  1인 경우)
-			if(Math.abs(col-i) == Math.abs(chessCol[col]-chessCol[i]))
-				return false;
-		}
-		//어느 부분도 겹치지 않으면
-		return true;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        board = new int[N]; //행 -> 인덱스, 열 -> 값
+
+        queen(0);
+
+        System.out.println(answer);
+
+    }
+
+    private static void queen(int depth) {
+        if(depth == N) {
+            answer++;
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            board[depth] = i;
+            if(check(depth)) {
+                queen(depth+1);
+            }
+
+        }
+
+    }
+    static boolean check(int depth) {
+        for (int i = 0; i < depth; i++) { //세로로 같은 칸이 있는지, 대각선에 위치 해 있는지
+            if(board[i] == board[depth] || (depth - i == Math.abs(board[depth] - board[i]))) return false;
+        }
+        return true;
+    }
 
 }

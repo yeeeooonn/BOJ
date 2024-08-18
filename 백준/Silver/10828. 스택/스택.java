7a0673f56b43b1697e212d9e3a88ca15b392f2ca
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2024-08-18 17:20:38 +0900

    [Silver IV] Title: 스택, Time: 128 ms, Memory: 18620 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		
+		
+		for(int i = 0; i < N; i ++) {
+			String[] command = br.readLine().split(" ");
+			
+			switch (command[0]) {
+	        	case "push":
+	        		stack.push(Integer.parseInt(command[1]));
+	        		break;
+		        case "pop":
+		            if (stack.empty()) {
+		                bw.write("-1" + "\n");
+		            } else {
+		                bw.write(stack.pop() + "\n");
+		            }
+		            break;
+		        case "size":
+		            bw.write(stack.size() + "\n");
+		            break;
+		        case "empty":
+		            if (stack.empty()) {
+		                bw.write("1" + "\n");
+		            } else {
+		                bw.write("0" + "\n");
+		            }
+		            break;
+		        case "top":
+		            if (stack.empty()) {
+		                bw.write("-1" + "\n");
+		            } else {
+		                bw.write(stack.peek() + "\n");
+		            }
+		            break;
+			}
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
